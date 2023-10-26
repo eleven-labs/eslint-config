230af141a45e9c67b27d18618fb110d010eb29b7
PASQUET Fabien <fab.pasquet@gmail.com>
2023-10-26 12:45:43 +0200

    feat: grouping packages eslint-config

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # About
 
-This is a npm workspaces project that shares **ESLint** and **Prettier** configurations for **JavaScript** and **TypeScript** projects.
+This is a npm workspaces project that shares **[ESLint](/packages/eslint-config)** and **[Prettier](/packages/prettier-config)** configurations for **JavaScript** and **TypeScript** projects.
 
 For more information, consider the `README.md` files of the packages.
--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -1,6 +1,9 @@
 module.exports = {
+  env: {
+    browser: true,
+  },
   extends: [
-    '@eleven-labs/eslint-config-typescript',
+    '@eleven-labs/eslint-config/base',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
@@ -17,6 +20,11 @@ module.exports = {
   rules: {
     'unicorn/prevent-abbreviations': ['error', {
       replacements: {
+        args: false,
+        arg: false,
+        env: false,
+        dev: false,
+        prod: false,
         prop: false,
         props: false,
         ref: false
